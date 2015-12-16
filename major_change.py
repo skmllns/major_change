54a6f604bfc98a937b475ec67aaa7a18d351ce87
skmullins <skmllns@gmail.com>
2015-12-16 14:50:36 -0500

    First 2 letter comparisons work, just need a loop now

--- major_change.py
+++ major_change.py
@@ -23,12 +23,10 @@ with open(write_file, 'wb') as wf:
       code = str(sheet.cell(row=row_num, column=64).value[0]) + str(sheet.cell(row=row_num, column=64).value[1])
       two_letters[code] = two_letters.get(code, 0) + 1
    for key in two_letters:
-      key_len = len(key)
       str_to_write = '['
+      key_len = 2
       for idx in range(key_len):
          if idx + 1 < key_len:
             str_to_write += '\'' + key[idx] + str(idx + 1) + '\', \'' + key[idx + 1] + str(idx + 2) + '\', '
-         str_to_write += str(two_letters[key]) + '],\n'
-         print str_to_write
-                
-      # wf.writelines(['[\'' + key[0] + '\', \'' + elem + '\', ' + str(all_codes[key]) + '],\n'])
+      str_to_write += str(two_letters[key]) + '],\n'
+      print str_to_write
