48434cd049aa91dc685850cd98823a3bea955f76
skmullins <skmllns@gmail.com>
2015-12-17 10:41:48 -0500

    Write to HTML file.

--- major_change.py
+++ major_change.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 import pandas as pd
 
 read_file = 'Major-School-College-Change 8.csv'
-write_file = 'table.txt'
+write_file = 'sankey.html'
 col_name = 'sch_seq_6'
 col_contents = []
 num_letters = 6
@@ -17,6 +17,24 @@ for cell in df[col_name]:
 
 #convert to google charts format
 with open(write_file, 'wb') as wf:
+   body = """<html>
+   <body>
+      <script type="text/javascript"
+      src="https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1.1','packages':['sankey']}]}">
+      </script>
+
+      <div id="sankey_multiple" style="width: 900px; height: 300px;"></div>
+
+      <script type="text/javascript">
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+          var data = new google.visualization.DataTable();
+          data.addColumn('string', 'From');
+          data.addColumn('string', 'To');
+          data.addColumn('number', 'Weight');
+          data.addRows(['
+   """
+   wf.write(body)
    for idx in range(num_letters): 
       codes = OrderedDict()
       for curr_cell in col_contents:
@@ -28,12 +46,28 @@ with open(write_file, 'wb') as wf:
          else:
             col_contents.remove(curr_cell)
       for key in codes:
-         str_to_write = '[\'' + key[0] + str(idx+1) + '\', \'' + key[1] + str(idx+2)+ '\', ' + str(codes[key]) + ']'
+         str_to_write = '\t\t\t\t\t[\'' + key[0] + str(idx+1) + '\', \'' + key[1] + str(idx+2)+ '\', ' + str(codes[key]) + ']'
          if idx + 1 == num_letters - 1 and codes.keys().index(key) == len(codes) - 1:
                pass
          else:
             str_to_write += ','
          str_to_write += '\n'
          wf.write(str_to_write)
+   footer = """      ]);
+
+    // Set chart options
+    var options = {
+      width: 600,
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.Sankey(document.getElementById('sankey_multiple'));
+    chart.draw(data, options);
+   }
+   </script>
+   </body>
+   </html>
+   """
+   wf.write(footer)
        
 print "Time to complete:" + str(time.time() - start_time)
